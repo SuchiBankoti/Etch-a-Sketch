1aa0f5c9b8a12fc8d7408dc574a8421408a4a94f
suchi <suchibankoti95@gmail.com>
2022-08-17 12:46:58 +0530

    back to flex for dynamic grid. making a separate branch for using css grid

--- sketch.js
+++ sketch.js
@@ -1,36 +1,43 @@
-let gridContainer = document.getElementById('container');
-const btn = document.querySelector('button');
-btn.addEventListener('click', gridMaker);
-
-
-function gridMaker() {
-  const n = 16;
-  for (let i = 0; i < n * n; i++) {
-    const div = document.createElement('div');
-    gridContainer.appendChild(div);
-    div.classList.add("grid")
-  }
-}
-
-gridContainer.addEventListener('mouseover', color);
-gridContainer.addEventListener('mouseout', (e) => e.target.style.backgroundColor = "rgb(245,245,245)");
-
-function color(e) {
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  let rgb = "rgb(" + r + "," + g + "," + b + ")";
-
-  e.target.style.backgroundColor = `${rgb}`;
-}
-
-
-
-
-//         
-//       
-//    
-//   function gridRemove(e) {
-//     while (gridContainer.firstChild) {
-//       gridContainer.removeChild(gridContainer.firstChild);
-//     }
+    const container = document.getElementById("container");
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', gridMaker);
+    function gridMaker(e) {
+      gridRemove(e);
+      const n = prompt();
+      for (let i = 0; i < n * n; i++) {
+        const div = document.createElement('div');
+        div.style.width = `${500 / n}px`;
+        div.classList.add("grid");
+        container.appendChild(div);
+  
+      }
+  
+    };
+    container.addEventListener
+      ('mouseover', function color(e) {
+        function rrggbb() {
+          let r = Math.floor(Math.random() * 256);
+          let g = Math.floor(Math.random() * 256);
+          let b = Math.floor(Math.random() * 256);
+          let color = "rgb(" + r + "," + g + "," + b + ")";
+          return color;
+        }
+        let gridColor = rrggbb();
+        e.target.style.backgroundColor = `${gridColor}`;
+  
+  
+  
+  
+        container.addEventListener('mouseout', function colorZero(e) {
+          e.target.style.backgroundColor = "rgb(245,245,245)";
+        })
+      });
+      const box=document.getElementsByClassName("grid");
+      box.addEventListener('click', function colorChange(e) {
+        e.target.style.backgroundColor = 'yellow';
+      });   // let change = function co(gridColor){
+    function gridRemove(e) {
+      while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    }
