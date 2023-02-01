9f6420249b165dabc779bf5415ad68596152e834
suchi <suchibankoti95@gmail.com>
2023-02-01 16:44:17 +0530

    add color

--- sketch.js
+++ sketch.js
@@ -1,43 +1,63 @@
-    const container = document.getElementById("container");
-    const btn = document.querySelector('button');
-    btn.addEventListener('click', gridMaker);
-    function gridMaker(e) {
-      gridRemove(e);
-      const n = prompt();
-      for (let i = 0; i < n * n; i++) {
-        const div = document.createElement('div');
-        div.style.width = `${500 / n}px`;
-        div.classList.add("grid");
-        container.appendChild(div);
-  
-      }
-  
-    };
-    container.addEventListener
-      ('mouseover', function color(e) {
-        function rrggbb() {
-          let r = Math.floor(Math.random() * 256);
-          let g = Math.floor(Math.random() * 256);
-          let b = Math.floor(Math.random() * 256);
-          let color = "rgb(" + r + "," + g + "," + b + ")";
-          return color;
-        }
-        let gridColor = rrggbb();
-        e.target.style.backgroundColor = `${gridColor}`;
-  
-  
-  
-  
-        container.addEventListener('mouseout', function colorZero(e) {
-          e.target.style.backgroundColor = "rgb(245,245,245)";
-        })
-      });
-      const box=document.getElementsByClassName("grid");
-      box.addEventListener('click', function colorChange(e) {
-        e.target.style.backgroundColor = 'yellow';
-      });   // let change = function co(gridColor){
-    function gridRemove(e) {
-      while (container.firstChild) {
-        container.removeChild(container.firstChild);
-      }
-    }
+const grid_container = document.getElementById("gridContainer");
+const btn_container = document.getElementById("btnContainer");
+const color_selector = document.getElementById("colorSelection");
+const grid_btn = document.getElementById("gridButton");
+const save_btn = document.createElement("button");
+
+let color = "";
+grid_btn.addEventListener("click", loadGrid);
+colorSelect();
+
+function loadGrid() {
+  gridMaker();
+  addSaveBtn();
+}
+function addSaveBtn() {
+  save_btn.textContent = "Save Grid";
+  btn_container.appendChild(save_btn);
+  save_btn.addEventListener("click", removeGrid);
+}
+function gridMaker() {
+  const n = prompt();
+  for (let i = 0; i < n * n; i++) {
+    const unit = document.createElement("div");
+    unit.style.width = `${500 / n}px`;
+    unit.classList.add("grid");
+    unit.addEventListener("click", addColorToGrid);
+    grid_container.appendChild(unit);
+  }
+}
+function removeGrid() {
+  alert("your sketch is saved");
+  const btn_container = document.getElementById("btnContainer");
+  btn_container.removeChild(save_btn);
+  while (grid_container.lastChild) {
+    grid_container.removeChild(grid_container.lastChild);
+  }
+}
+
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+function addColorToGrid(e) {
+  e.target.style.backgroundColor = color;
+}
+
+function colorSelect() {
+  const label = document.createElement("label");
+  label.textContent = "Select your color : ";
+  const select = document.createElement("select");
+  select.addEventListener("change", (e) => {
+    color = e.target.value;
+  });
+  for (let i = 0; i < 20; i++) {
+    let color = randomColor();
+    const option = document.createElement("option");
+    option.style.backgroundColor = color;
+    option.textContent = color;
+    option.value = color;
+    select.appendChild(option);
+  }
+  label.appendChild(select);
+  color_selector.appendChild(label);
+}
